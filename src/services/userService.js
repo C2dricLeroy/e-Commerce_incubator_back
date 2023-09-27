23d080993ab3d6ee43ac75f31b84ea2bc6e757ea
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-27 19:42:25 +0100

    feat(user): add and configure bcrypt for password storage

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 class UserService {
@@ -7,15 +8,14 @@ class UserService {
       const { username, email, password } = req.body;
       const date = new Date();
       const uuid = uuidv4();
-
+      const hash = await bcrypt.hash(password, 10);
       const newUser = await User.create({
         uuid,
         username,
-        password,
+        password: hash,
         email,
         datetime: date,
       });
-
       res.status(201).json(newUser);
     } catch (error) {
       console.log(error.message);
