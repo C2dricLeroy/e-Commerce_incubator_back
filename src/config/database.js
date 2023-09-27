a7018ef66594c132fc8009af18179d0c0865a09e
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-27 15:22:55 +0100

    feat(sequelize): add and setup sequelize

---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+
+// Remplacez ces valeurs par les informations de votre base de données PostgreSQL
+const databaseName = 'postgres';
+const databaseUser = 'e-commerce';
+const databasePassword = 'e-commerce';
+const databaseHost = 'localhost';
+const databasePort = 5666;
+
+const sequelize = new Sequelize(databaseName, databaseUser, databasePassword, {
+  host: databaseHost,
+  port: databasePort,
+  dialect: 'postgres',
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connexion à la base de données établie avec succès.');
+  })
+  .catch((error) => {
+    console.error('Impossible de se connecter à la base de données :', error);
+  });
+
+module.exports = sequelize;
