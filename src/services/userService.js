86438fc4181154667b4fb271ff5df099cb03342a
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-10-02 19:58:01 +0100

    feat(user): add conditions and verification for user's creation

--- src/services/userService.js
+++ src/services/userService.js
@@ -7,25 +7,55 @@ class UserService {
     this.prisma = new PrismaClient();
   }
 
+  isValidPassword(password) {
+    const hasSpecialCharacter = /[^a-zA-Z0-9\s]/.test(password);
+    const hasDigit = /\d/.test(password);
+    const hasUppercase = /[A-Z]/.test(password);
+    if (this && (!hasSpecialCharacter || !hasDigit || !hasUppercase)) {
+      console.error('Invalid password');
+      return false;
+    }
+    return password.length > 8;
+  }
+
   async createUser(req, res) {
-    try {
-      const { username, email, password } = req.body;
-      const date = new Date();
-      const uuid = uuidv4();
-      const hash = await bcrypt.hash(password, 10);
-      const newUser = await this.prisma.user.create({
-        data: {
-          uuid,
-          username,
-          password: hash,
-          email,
-          datetime: date,
-        },
-      });
-      res.status(201).json(newUser);
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).json({ error: 'Une erreur s\'est produite lors de la création de l\'utilisateur' });
+    const { username, email, password } = req.body;
+    const date = new Date();
+    const uuid = uuidv4();
+
+    if (!this.isValidPassword(password)) {
+      throw new Error('Invalid password');
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.findUnique({
+      select: {
+        email: true,
+      },
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw new Error('Email already used');
+    } else {
+      try {
+        const newUser = await this.prisma.user.create({
+          data: {
+            uuid,
+            username,
+            password: hash,
+            email,
+            datetime: date,
+          },
+        });
+        res.status(201).json(newUser);
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ error: 'Une erreur s\'est produite lors de la création de l\'utilisateur' });
+      }
     }
   }
 }
