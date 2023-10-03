aef97950afb2d94f8d0472be1f96c519e47445b9
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-10-03 11:17:21 +0100

    feat(password): implement data validation on password for user's creation

--- src/models/validator.js
+++ src/models/validator.js
@@ -3,7 +3,13 @@ const yup = require('yup');
 const userSchema = yup.object({
   username: yup.string().required(),
   email: yup.string().email().required(),
-  password: yup.string().required(),
+  password: yup
+    .string()
+    .required('Le mot de passe est requis.')
+    .min(8, 'Le mot de passe doit comporter au moins 8 caractères.')
+    .max(255, 'Le mot de passe ne peut pas dépasser 255 caractères.')
+    .matches(/[A-Z]/, 'Le mot de passe doit contenir au moins une lettre majuscule.')
+    .matches(/[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/, 'Le mot de passe doit contenir au moins un caractère spécial.'),
 });
 
 module.exports = userSchema;
